3053aeb84a65e81f52f650112c4f2720cbb3191b
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-06-18 21:33:10 +0100

    adiciona o painel de secretariado

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\MembroPanelProvider::class,
+    App\Providers\Filament\SecretariadoPanelProvider::class,
 ];
